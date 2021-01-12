ee10f858cebaf146a9c26ec8bccb7a6deb25feba
vycius <k.vycius@gmail.com>
2021-01-12 14:09:21 +0000

    Auto generated NephroLog API client

---
+++ test/user_api_test.dart
@@ -0,0 +1,32 @@
+import 'package:nephrogo_api_client/api.dart';
+import 'package:nephrogo_api_client/api/user_api.dart';
+import 'package:test/test.dart';
+
+
+/// tests for UserApi
+void main() {
+  final instance = NephrogoApiClient().getUserApi();
+
+  group(UserApi, () {
+    //Future<UserProfile> userProfileCreate(UserProfileRequest userProfileRequest) async
+    test('test userProfileCreate', () async {
+      // TODO
+    });
+
+    //Future<UserProfile> userProfilePartialUpdate(UserProfileRequest userProfileRequest) async
+    test('test userProfilePartialUpdate', () async {
+      // TODO
+    });
+
+    //Future<UserProfile> userProfileRetrieve() async
+    test('test userProfileRetrieve', () async {
+      // TODO
+    });
+
+    //Future<UserProfile> userProfileUpdate(UserProfileRequest userProfileRequest) async
+    test('test userProfileUpdate', () async {
+      // TODO
+    });
+
+  });
+}
