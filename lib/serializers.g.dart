88646953e705dd62ef031c6de549afb882991eaa
vycius <k.vycius@gmail.com>
2021-02-07 19:27:40 +0000

    Auto generated NephroLog API client

---
+++ lib/serializers.g.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of serializers;
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+Serializers _$serializers = (new Serializers().toBuilder()
+      ..add(AppetiteEnum.serializer)
+      ..add(ChronicKidneyDiseaseStageEnum.serializer)
+      ..add(DailyHealthStatus.serializer)
+      ..add(DailyHealthStatusRequest.serializer)
+      ..add(DailyIntakesLightReport.serializer)
+      ..add(DailyIntakesReport.serializer)
+      ..add(DailyIntakesReportResponse.serializer)
+      ..add(DailyIntakesReportsResponse.serializer)
+      ..add(DailyNutrientConsumption.serializer)
+      ..add(DailyNutrientNormsWithTotals.serializer)
+      ..add(DiabetesComplicationsEnum.serializer)
+      ..add(DiabetesTypeEnum.serializer)
+      ..add(DialysisTypeEnum.serializer)
+      ..add(GenderEnum.serializer)
+      ..add(HealthStatusScreenResponse.serializer)
+      ..add(HealthStatusWeeklyScreenResponse.serializer)
+      ..add(Intake.serializer)
+      ..add(IntakeRequest.serializer)
+      ..add(NutrientScreenResponse.serializer)
+      ..add(NutrientWeeklyScreenResponse.serializer)
+      ..add(Product.serializer)
+      ..add(ProductKindEnum.serializer)
+      ..add(ProductRequest.serializer)
+      ..add(ProductSearchResponse.serializer)
+      ..add(ShortnessOfBreathEnum.serializer)
+      ..add(Swelling.serializer)
+      ..add(SwellingDifficultyEnum.serializer)
+      ..add(SwellingEnum.serializer)
+      ..add(SwellingRequest.serializer)
+      ..add(User.serializer)
+      ..add(UserAppReview.serializer)
+      ..add(UserProfile.serializer)
+      ..add(UserProfileRequest.serializer)
+      ..add(UserRequest.serializer)
+      ..add(WellFeelingEnum.serializer)
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(DailyHealthStatus)]),
+          () => new ListBuilder<DailyHealthStatus>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(DailyHealthStatus)]),
+          () => new ListBuilder<DailyHealthStatus>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(DailyIntakesLightReport)]),
+          () => new ListBuilder<DailyIntakesLightReport>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(DailyIntakesReport)]),
+          () => new ListBuilder<DailyIntakesReport>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(DailyIntakesReport)]),
+          () => new ListBuilder<DailyIntakesReport>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(Intake)]),
+          () => new ListBuilder<Intake>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(Intake)]),
+          () => new ListBuilder<Intake>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(Product)]),
+          () => new ListBuilder<Product>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(Swelling)]),
+          () => new ListBuilder<Swelling>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(SwellingRequest)]),
+          () => new ListBuilder<SwellingRequest>()))
+    .build();
+
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
